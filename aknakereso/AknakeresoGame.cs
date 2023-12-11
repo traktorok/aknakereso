3638dd5491857630bb27886a41633415c5550dd6
arongeo <58132897+arongeo@users.noreply.github.com>
2023-12-11 22:23:39 +0100

    Rekurziv 0 erteku mezo felfedo eljaras, meg dokumentacio, meg nem tudom meg mi

--- aknakereso/AknakeresoGame.cs
+++ aknakereso/AknakeresoGame.cs
@@ -7,28 +7,31 @@ using System.Threading.Tasks;
 
 namespace aknakereso
 {
+    /// <summary>
+    /// Egy ketdimenzios koordinata
+    /// x es y ertekkel.
+    /// </summary>
     struct Coordinate
     {
         public int x;
         public int y;
-
-        public Coordinate(int param_x, int param_y)
-        {
-            x = param_x;
-            y = param_y;
-        }
     }
 
-    struct Square {
+    /// <summary>
+    /// Egy adatstruktura, amely tartalmazza
+    /// a mezo erteket, es hogy fel van-e fedve,
+    /// vagy nem.
+    /// </summary>
+    struct Mezo {
         public int value;
         public bool revealed;
     }
 
     class AknakeresoGame
     {
-        private Coordinate curPos;
-        private Coordinate boardSize;
-        private Square[,] board;
+        private Coordinate curPos = new Coordinate();
+        private Coordinate tablaSize = new Coordinate();
+        private Mezo[,] tabla;
 
         private bool elsoReveal = true;
 
@@ -38,171 +41,165 @@ namespace aknakereso
         /// Megnezi, hogy az x es y koordinata a 
         /// jatektablan belul tartozkodik-e.
         /// </summary>
-        /// <param name="x">X koordinata, amit meg kell neznunk</param>
-        /// <param name="y">Y koordinata, amit meg kell neznunk</param>
-        /// <returns></returns>
+        /// <param name="x">X koordinata, amit ellenoriznunk kell</param>
+        /// <param name="y">Y koordinata, amit ellenoriznunk kell</param>
+        /// <returns>Benne vannak-e a koordinatak a jatekterben, vagy nem</returns>
         private bool CheckCoordInBounds(int x, int y)
         {
-            if (0 > x || boardSize.x <= x)
+            if (0 > x || tablaSize.x <= x)
             {
                 return false;
             }
 
-            if (0 > y || boardSize.y <= y)
+            if (0 > y || tablaSize.y <= y)
             {
                 return false;
             }
 
             return true;
         }
-
-        public void PlaceAkna(int x, int y)
-        {
-            board[y, x].value = AKNA;
-            for (int i = y - 1; i <= y + 1; i++)
-            {
-                for (int j = x - 1; j <= x + 1; j++)
-                {
-                    if (!CheckCoordInBounds(j, i))
-                    {
-                        continue;
-                    }
-
-                    if (board[i, j].value != AKNA && !(i == y && j == x))
-                    {
-                        board[i, j].value++;
-                    }
-                }
-            }
-        }
-
-        private void GenerateAknak(int aknakSzama, int startPosX, int startPosY)
-        {
-            Random rnd = new Random();
-
-            int x, y;
-            int i = 0;
-            while (i < aknakSzama)
-            {
-                x = rnd.Next(0, boardSize.x);
-                y = rnd.Next(0, boardSize.y);
-
-                if (startPosX == x && startPosY == y)
-                {
-                    i++;
-                    continue;
-                }
-
-                if (board[y, x].value != AKNA)
-                {
-                    PlaceAkna(x, y);
-                    i++;
-                }
-            }
-        }
-
-        public void WriteSquare(int x, int y)
+        
+        /// <summary>
+        /// Kiirja az X, Y koordinatan elhelyezkedo mezo erteket, az ertektol,
+        /// es attol fuggoen, hogy fel van-e fedve.
+        /// </summary>
+        /// <param name="x">Az X koordinata</param>
+        /// <param name="y">Az Y koordinata</param>
+        public void WriteMezo(int x, int y)
         {
             if (!CheckCoordInBounds(x, y))
                 return;
 
-            if (board[y, x].revealed == false)
+            if (tabla[y, x].revealed == false)
             {
                 Console.Write('#');
             }
             else
             {
-                if (board[y, x].value == 10)
+                if (tabla[y, x].value == 10)
                 {
                     Console.Write('*');
                 }
-                else if (board[y, x].value == 0)
+                else if (tabla[y, x].value == 0)
                 {
                     Console.Write(' ');
                 }
-                else if (board[y, x].value < 10)
+                else if (tabla[y, x].value < 10)
                 {
-                    Console.Write(board[y, x].value.ToString());
+                    Console.Write(tabla[y, x].value.ToString());
                 }
             }
         }
 
-        public void RenderBoard()
+        /// <summary>
+        /// Letorli a konzolt, majd ujrarajzolja a teljes tablat.
+        /// </summary>
+        public void RenderTabla()
         {
+            Console.Clear();
             Console.SetCursorPosition(0, 0);
 
-            curPos.x = 0;
-            curPos.y = 0;
-
-            for (int y = 0; y < boardSize.y; y++)
+            for (int y = 0; y < tablaSize.y; y++)
             {
-                for (int x = 0; x < boardSize.x; x++)
+                for (int x = 0; x < tablaSize.x; x++)
                 {
-                    WriteSquare(x, y);
+                    WriteMezo(x, y);
                 }
                 Console.Write('\n');
             }
+
+            Console.SetCursorPosition(curPos.x, curPos.y);
         }
 
-        public AknakeresoGame(int boardWidth, int boardHeight, int difficulty)
+        /// <summary>
+        /// Az AknakeresoGame osztaly konstruktore, inicializalja
+        /// a ketdimenzios tabla tombot es a curPos adatstrukturat,
+        /// beallitja a konzol tulajdonsagokat.
+        /// </summary>
+        /// <param name="tablaWidth">A tabla szelessege</param>
+        /// <param name="tablaHeight">A tabla magassaga</param>
+        /// <param name="difficulty">A nehezseg (az aknak szamat befolyasolja)</param>
+        public AknakeresoGame(int tablaWidth, int tablaHeight, int difficulty)
         {
             curPos.x = 0;
             curPos.y = 0;
-            boardSize.x = boardWidth;
-            boardSize.y = boardHeight;
-            board = new Square[boardHeight, boardWidth];
+            tablaSize.x = tablaWidth;
+            tablaSize.y = tablaHeight;
+            tabla = new Mezo[tablaHeight, tablaWidth];
             Console.CursorVisible = false;
 
-            RenderBoard();
+            RenderTabla();
         }
         
-        private void RenderCurrSor()
+        /// <summary>
+        /// Kirajzolja (vagy felulirja onmagaval) az Y sorszamu sort.
+        /// </summary>
+        /// <param name="y">A sor szama, amit ki kell rajzolni</param>
+        private void RenderSor(int y)
         {
-            for (int i = 0; i < boardSize.x; i++)
+            for (int i = 0; i < tablaSize.x; i++)
             {
-                Console.SetCursorPosition(i, curPos.y);
-                WriteSquare(i, curPos.y);
+                Console.SetCursorPosition(i, y);
+                WriteMezo(i, y);
             }
             
             Console.SetCursorPosition(curPos.x, curPos.y);
 
             Console.BackgroundColor = ConsoleColor.Blue;
-            WriteSquare(curPos.x, curPos.y);
+            WriteMezo(curPos.x, curPos.y);
             Console.BackgroundColor = ConsoleColor.Black;
         }
 
-        public int MoveCursor(int x, int y)
+        /// <summary>
+        /// Ha elmozdulas tortenik, elmozditja a kurzort es ujrarajzolja
+        /// azt a sort, amelybol elmozdult, es azt is amelybe elmozdult.
+        /// Ha nem tortenik elmozdulas, csak ujrarajzolja a jelenlegi sort.
+        /// Az negativ elmozdulas az X tengelyen balra, az Y-on felfele mozditja
+        /// el a kurzort.
+        /// A pozitiv elmozdulas pedig az X tengelyen jobbra, az Y-on lefele mozgat.
+        /// </summary>
+        /// <param name="xChange">Az elvaltozas merteke az X tengelyen</param>
+        /// <param name="yChange">Az elvaltozas merteke az Y tengelyen</param>
+        public void MoveCursor(int xChange, int yChange)
         {
-            RenderCurrSor();
+            RenderSor(curPos.y);
 
             // Csak akkor valtoztatjuk a kurzort, ha az
             // a tabla teruleten belul van
-            if (CheckCoordInBounds(x + curPos.x, y + curPos.y))
+            if (CheckCoordInBounds(xChange + curPos.x, yChange + curPos.y))
             {
                 Console.SetCursorPosition(curPos.x, curPos.y);
-                WriteSquare(curPos.x, curPos.y);
+                WriteMezo(curPos.x, curPos.y);
 
-                curPos.x += x;
-                curPos.y += y;
+                curPos.x += xChange;
+                curPos.y += yChange;
 
                 Console.SetCursorPosition(curPos.x, curPos.y);
 
                 Console.BackgroundColor = ConsoleColor.Blue;
-                WriteSquare(curPos.x, curPos.y);
+                WriteMezo(curPos.x, curPos.y);
                 Console.BackgroundColor = ConsoleColor.Black;
             }
-
-            return 0;
         }
 
-        public bool CheckWin()
+        /// <summary>
+        /// Vegigmegy az egesz tablan, es ellenorzi, hogy a lefedett mezok kozott
+        /// mar csak aknak vannak-e?
+        /// </summary>
+        /// <returns>
+        /// Igazat ad vissza, ha a jatekos minden aknat ki tudott kerulni,
+        /// es felfedett minden olyan mezot, amelyen nincs akna.
+        /// Hamisat ad vissza, ha van meg olyan lefedett mezo, amin nem akna
+        /// van.
+        /// </returns>
+        private bool CheckWin()
         {
             bool didWin = true;
-            for (int x = 0; x < boardSize.x; x++)
+            for (int x = 0; x < tablaSize.x; x++)
             {
-                for (int y = 0; y < boardSize.y; y++)
+                for (int y = 0; y < tablaSize.y; y++)
                 {
-                    if (board[y, x].value != AKNA && board[y, x].revealed == false)
+                    if (tabla[y, x].value != AKNA && tabla[y, x].revealed == false)
                     {
                         didWin = false;
                     }
@@ -212,20 +209,134 @@ namespace aknakereso
             return didWin;
         }
 
+        /// <summary>
+        /// Egy rekurziv eljaras, ami az egymas mellett levo
+        /// ures mezoket (0 erteku) felfedi, amennyiben az egyiket kozuluk felfedjuk.
+        /// </summary>
+        /// <param name="x">A felfedendo ures mezo koordinataja az X tengelyen. (hanyadik oszlop?)</param>
+        /// <param name="y">A felfedendo ures mezo koordinataja az Y tengelyen. (hanyadik sor?)</param>
+        private void RevealEmpty(int x, int y)
+        {
+            if (CheckCoordInBounds(x, y))
+            {
+                if (tabla[y, x].value != AKNA && tabla[y, x].revealed == false)
+                {
+                    tabla[y, x].revealed = true;
+
+                    if (tabla[y, x].value == 0)
+                    {
+                        for (int i = y - 1; i <= y + 1; i++)
+                        {
+                            for (int j = x - 1; j <= x + 1; j++)
+                            {
+                                if (!CheckCoordInBounds(j, i))
+                                {
+                                    continue;
+                                }
+
+                                // Sajat koordinatain ne hivja ujra,
+                                // hiszen az egy orok loop.
+                                if (!(i == y && j == x))
+                                {
+                                    RevealEmpty(j, i);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Elhelyez egy aknat az X, Y koordinatan levo mezon,
+        /// es a korulotte levo mezok ertekeit noveli 1-gyel.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void PlaceAkna(int x, int y)
+        {
+            tabla[y, x].value = AKNA;
+            for (int i = y - 1; i <= y + 1; i++)
+            {
+                for (int j = x - 1; j <= x + 1; j++)
+                {
+                    if (!CheckCoordInBounds(j, i))
+                    {
+                        continue;
+                    }
+
+                    if (tabla[i, j].value != AKNA && !(i == y && j == x))
+                    {
+                        tabla[i, j].value++;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Elhelyez megadott szamu aknat random koordinataju mezokre, amig a
+        /// random mezo nem a start mezo (azaz a jatekos altal
+        /// valasztott elso mezo).
+        /// </summary>
+        /// <param name="aknakSzama">A lehelyezendo aknak szama</param>
+        /// <param name="startPosX">A start mezo X koordinataja (oszlop)</param>
+        /// <param name="startPosY">A start mezo Y koordinataja (sor)</param>
+        private void GenerateAknak(int aknakSzama, int startPosX, int startPosY)
+        {
+            Random rnd = new Random();
+
+            int x, y;
+            int i = 0;
+            while (i < aknakSzama)
+            {
+                x = rnd.Next(0, tablaSize.x);
+                y = rnd.Next(0, tablaSize.y);
+
+                if (startPosX == x && startPosY == y)
+                {
+                    i++;
+                    continue;
+                }
+
+                // Ketszer nem rakunk ugyanoda aknat
+                if (tabla[y, x].value != AKNA)
+                {
+                    PlaceAkna(x, y);
+                    i++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Felfedi azt a mezot, amelyen jelenleg a kurzor all.
+        /// Amennyiben ez az elso felfedes, legeneralja az aknak poziciojat.
+        /// Amennyiben a felfedendo mezo egy akna, a jatekos veszit.
+        /// Amennyiben az az utolso nem akna felfedheto mezo, a jatekos nyer.
+        /// Egyebkent pedig ujrarajzolja a jelenlegi sort.
+        /// </summary>
+        /// <returns>
+        /// -1 ha a jatekos vesztett, 1 ha nyert, 0 ha folytatodik a jatek.
+        /// </returns>
         public int RevealCurPos()
         {
-            Square currSquare;
-            currSquare = board[curPos.y, curPos.x];
-
             // Az aknageneralas az elso mezo kivalasztasakor tortenik,
             // hogy biztosak legyunk abban, hogy azon nincs akna.
             if (elsoReveal == true)
             {
-                GenerateAknak(10, curPos.x, curPos.y);
+                GenerateAknak(8, curPos.x, curPos.y);
                 elsoReveal = false;
             }
 
-            if (currSquare.value == AKNA && currSquare.revealed == false)
+            Mezo currMezo;
+            currMezo = tabla[curPos.y, curPos.x];
+
+            if (currMezo.value == 0 && currMezo.revealed == false)
+            {
+                RevealEmpty(curPos.x, curPos.y);
+                RenderTabla();
+            }
+
+            if (currMezo.value == AKNA && currMezo.revealed == false)
             {
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.Clear();
@@ -234,10 +345,10 @@ namespace aknakereso
                 return -1;
             }
 
-            board[curPos.y, curPos.x].revealed = true;
+            tabla[curPos.y, curPos.x].revealed = true;
             // Azert az egesz sort rendereljuk ujra, mivel
             // a ReadKey felulirhat karaktereket a sorban.
-            RenderCurrSor();
+            RenderSor(curPos.y);
 
             if (CheckWin())
             {
